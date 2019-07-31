f0ee1c9b9e9a4100e57f9eb5fdd2baecec5ff102
Mark Fransen <swiftymf@gmail.com>
2019-07-31 11:16:04 -0700

    Wire up basic tableView function. Add searchBar.

--- NomadMaster/ResultsViewController.swift
+++ NomadMaster/ResultsViewController.swift
@@ -8,20 +8,39 @@
 
 import UIKit
 
-class ResultsViewController: UIViewController {
+class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var cellTextForFunsies = "Hello, I'm a cell"
+    var cellDetailTextForFunsies = "I'm also a superstar!"
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.barTintColor = .white
+    }
 
+}
 
-
-
-
+extension ResultsViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = cellTextForFunsies
+        cell.detailTextLabel?.text = cellDetailTextForFunsies
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell at \(indexPath.row) tapped")
+    }
 }
